4fca38b3d709a483a37b69c47fb6b6f04753d537
O. Barge <o_barge@Os-5k-iMac.local>
2019-12-08 04:58:27 -0700

    Initial-Commit

---
+++ _hello.py
@@ -0,0 +1,23 @@
+import math
+import sys
+
+from os import rename
+
+# third party package was sorted separately.
+import requests
+
+# # print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
+
+# example displays input using terminal.
+# name = input("Your Name? ")
+# print("Hello,", name)
